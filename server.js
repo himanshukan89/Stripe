862a6569cbb37a35e983448454919a14917e61df
himanshukan89 <himanshukan89@gmail.com>
2024-08-04 12:12:21 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const stripe = require('stripe')('your stripe server key here');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 app.use(bodyParser.json());
